b28c1dbbeb0c2b764ea4850d0ad1f1a2de0c2632
hannahashe <hannahashe13@gmail.com>
2026-05-09 00:23:10 +0100

    Register ConnectionNote model in admin with custom admin interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ConnectionNote
 
-# Register your models here.
+
+@admin.register(ConnectionNote)
+class ConnectionNoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "category",
+        "approximate_location_label",
+        "door_left_open",
+        "approved",
+        "created_at",
+    )
+    list_filter = ("approved", "category", "door_left_open", "created_at")
+    search_fields = ("title", "body", "approximate_location_label")
+    list_editable = ("approved",)
